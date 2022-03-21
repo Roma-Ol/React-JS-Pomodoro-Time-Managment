ee4353c849795923e644ab320267a3fb766d8464
RomaRoma <r.oliievskyi@internetdevels.com>
2022-03-21 21:11:23 +0200

    [1] Created countown + setting form

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,23 +2,36 @@ import '../App.css';
 
 import {Component} from "react";
 import Header from "./Header/Header";
+import Preferences from "./Preferences/Preferences";
 import Timer from "./Timer/Timer";
 import Footer from "./Footer/Footer";
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            x: 0,
+            startPoint: 25,
         }
     }
 
+    changePreference = (time) => {
+        this.setState({
+            startPoint: time
+        })
+    }
+
     render() {
+        const time = new Date();
+
+
         return (
             <div className="App">
-                {/*<Confetti/>*/}
                 <Header/>
-                <Timer/>
+                <Preferences
+                    changePreference={this.changePreference}
+                    defaultValue={this.state.startPoint}/>
+                <Timer time={this.state.startPoint}/>
                 <Footer/>
             </div>
         );
